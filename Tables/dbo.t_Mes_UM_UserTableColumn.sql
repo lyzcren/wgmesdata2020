196a28f1774547bb6556ce7eb1b120f898f68cf7
lyzcren <328043783@qq.com>
2020-05-30 16:35:29 +0800

    配置列表格长度由整型改为浮点型

--- Tables/dbo.t_Mes_UM_UserTableColumn.sql
+++ Tables/dbo.t_Mes_UM_UserTableColumn.sql
@@ -6,7 +6,7 @@ CREATE TABLE [dbo].[t_Mes_UM_UserTableColumn]
 [FDataIndex] [nvarchar] (50) COLLATE Chinese_PRC_CI_AS NOT NULL,
 [FIsHidden] [bit] NOT NULL CONSTRAINT [DF_t_Mes_UM_UserTableColumns_FIsHidden] DEFAULT ((0)),
 [FisAuthorized] [bit] NOT NULL CONSTRAINT [DF_t_Mes_UM_UserTableColumns_FisAuthorized] DEFAULT ((1)),
-[FWidth] [int] NOT NULL CONSTRAINT [DF_t_Mes_UM_UserTableColumns_FWidth] DEFAULT ((0))
+[FWidth] [decimal] (18, 4) NOT NULL CONSTRAINT [DF_t_Mes_UM_UserTableColumns_FWidth] DEFAULT ((0))
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[t_Mes_UM_UserTableColumn] ADD CONSTRAINT [PK_t_Mes_UM_UserTableColumns] PRIMARY KEY CLUSTERED  ([FInterID], [FEntryID]) ON [PRIMARY]
